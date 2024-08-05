ba084f418ce413a7f41c027578d24cd0c7beb196
RzvST <stanrazvan06@gmail.com>
2024-08-05 23:53:38 +0300

    updated commands

--- serverprep.sh
+++ serverprep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Check and install updates"
-    sudo apt update && sudo apt upgrade
+    sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean -y
 echo "Updates done"
