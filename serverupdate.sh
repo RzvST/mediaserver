725216b4707b458586fcc2692bb42fbf5adc4f60
RzvST <stanrazvan06@gmail.com>
2024-08-06 00:28:41 +0300

    Added scripts

--- serverupdate.sh
+++ serverupdate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "****************  Check and install updates **************** "
+echo "****************  Check and install updates ****************"
     sudo apt update && \
     sudo apt upgrade -y && \
     sudo apt autoremove -y && \
     sudo apt autoclean -y
-echo "**************** Updates done **************** "
+echo "**************** Updates done ****************"
