da932f6e34efdea5481c83946929b0b0945211a3
RzvST <stanrazvan06@gmail.com>
2024-08-08 22:01:36 +0300

    scripts update

---
+++ scripts/portainer_stacks.sh
@@ -0,0 +1,11 @@
+echo "****************  Download Portainer Stacks File ****************"
+
+wget https://raw.githubusercontent.com/RzvST/mediaserver/main/portainer_stacks/mediaserver.yml
+
+echo "**************** Create and Copy Portainer Configuration ****************"
+
+## /var/lib/docker/volumes/portainer_data/_data/compose/1
+
+sudo mkdir /var/lib/docker/volumes/portainer_data/_data/compose/1
+
+sudo cp mediaserver.yml /var/lib/docker/volumes/portainer_data/_data/compose/1
