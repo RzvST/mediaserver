e3ca081d5a0c11dde428090388ced86fab15b8bd
RzvST <stanrazvan06@gmail.com>
2024-08-08 19:39:07 +0300

    script folder

--- serverupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "****************  Check and install updates ****************"
-    sudo apt update && \
-    sudo apt upgrade -y && \
-    sudo apt autoremove -y && \
-    sudo apt autoclean -y
-echo "**************** Updates done ****************"
