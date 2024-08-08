8dd1ca92b11ec8ca0c5b49023b14abf8de24544d
RzvST <stanrazvan06@gmail.com>
2024-08-08 19:50:04 +0300

    no interaction added

--- scripts/dockersetup.sh
+++ scripts/dockersetup.sh
@@ -26,4 +26,4 @@ sudo apt-get update
 
 echo "****************  Install the Docker packages ****************"
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
